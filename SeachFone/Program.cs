0dd62f2697ba14c05f8def1ee395e60f6f783a48
Charles Gomes <charles.kratos@gmail.com>
2020-01-29 22:45:18 -0400

    insercao do metodo de verificacao de numero

--- SeachFone/Program.cs
+++ SeachFone/Program.cs
@@ -3,7 +3,10 @@
 namespace SeachFone {
     class MainClass {
         public static void Main(string[] args) {
-
+            string dados = "O numero de celular da Ana: (95) 99167-8545";
+            RegexFactory factory = new RegexFactory();
+            var numeros = factory.GetResult(dados);
+            numeros.ForEach(Console.WriteLine);
         }
     }
 }
